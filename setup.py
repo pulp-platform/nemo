567e514bf08875fa9f7ce7caf41968b46d2ca655
Francesco Conti <FrancescoConti@users.noreply.github.com>
2020-04-09 12:58:19 +0200

    Reorganized nemo as a PyPI package (#3)
    * Reorganized nemo as a PIP package
    
    * Added install_requires requirements to setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NEMO",
+    version="0.1",
+    author="Francesco Conti",
+    author_email="f.conti@unibo.it",
+    description="NEural Minimizer for pytOrch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pulp-platform/nemo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        "torch>=1.3.0",
+        "torchvision>=0.4.1",
+        "numpy",
+        "tqdm",
+        "packaging"
+    ]
+)
