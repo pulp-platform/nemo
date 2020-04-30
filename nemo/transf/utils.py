ac6c8f0fd8c764fd690c0bd78fc8a1442c9c9820
Francesco Conti <f.conti@unibo.it>
2020-04-30 19:05:46 +0200

    Move qd_stage, id_stage from transf.utils -> transf.deploy

--- nemo/transf/utils.py
+++ nemo/transf/utils.py
@@ -153,22 +153,3 @@ def _reset_alpha_weights_pact(self, **kwargs):
             m.__class__.__name__ == "PACT_Linear"):
             m.reset_alpha_weights(**kwargs)
 
-def _qd_stage(self, eps_in=None, add_input_bias_dict={}, remove_bias_dict={}, **kwargs):
-    self.harden_weights()
-    if add_input_bias_dict:
-        self.add_input_bias(add_input_bias_dict)
-    if remove_bias_dict:
-        self.remove_bias(remove_bias_dict)
-    nemo.transform.bn_quantizer(self, **kwargs)
-    # harden_weights repeated to harden also BN parameters
-    self.harden_weights()
-    self.set_deployment(eps_in=eps_in)
-
-def _id_stage(self, eps_in=None, **kwargs):
-    if self.stage == 'fq':
-        self.qd_stage(eps_in=eps_in, **kwargs)
-    self.stage = 'id'
-    if eps_in is None:
-        eps_in = self.eps_in
-    nemo.transform.integerize_pact(self, eps_in=eps_in)
-
